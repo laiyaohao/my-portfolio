abce7c067228d8d6f019f0e40c33f1d7a96bb91e
laiyaohao <laiyaohao96@gmail.com>
2022-02-17 00:05:34 +0800

    refactor: remove the frontend code responsible for posts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from '../Home/Home'
 import About from '../About/About'
-import SinglePost from '../SinglePost/SinglePost'
-import Posts from '../Posts/Posts'
 import Projects from '../Projects/Projects'
 import NavBar from '../NavBar/NavBar'
 import SocialBar from '../SocialBar/SocialBar'
@@ -14,8 +12,6 @@ function App() {
       <Switch>
         <Route component={Home} path="/" exact />
         <Route component={About} path="/about" />
-        <Route component={SinglePost} path="/posts/:slug" />
-        <Route component={Posts} path="/posts" />
         <Route component={Projects} path="/projects" />
       </Switch>
       <SocialBar />

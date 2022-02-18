fb2d7dba99d8b8d3f93203587846cbbe1117c4ff
laiyaohao <laiyaohao96@gmail.com>
2022-02-18 23:37:14 +0800

    refactor: remove link to blog posts

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -23,11 +23,6 @@ export default function NavBar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" to="/posts">
-                Blog Posts
-              </NavLink>
-            </li>
-            <li className="nav-item">
               <NavLink className="nav-link" to="/projects">
                 Projects
               </NavLink>
